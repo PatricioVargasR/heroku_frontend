19146a415d886d0deb598f5eb4d961bd391e2035
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-10-30 10:02:29 -0600

    ADD URL TO RESPONSE

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,36 +1,35 @@
-function getAll(){
-    var request = new XMLHttpRequest;
-    //request.open('GET',"http://localhost:8000/contactos");
-    request.open('GET',"https://herokubackendsql-03fb6209ab45.herokuapp.com/contactos");
+function getAll() {
+    var request = new XMLHttpRequest();
+    //request.open('GET', "http://localhost:8000/contactos");
+    request.open('GET', "https://herokubackendsql-03fb6209ab45.herokuapp.com/contactos");
     request.send();
 
     request.onload = (e) => {
         const response = request.responseText;
         const json = JSON.parse(response);
-        console.log("response: " + response);
-        console.log("json: " + json);
         console.log("status_code: " + request.status);
-
-        console.log("Email: " + json[0]["email"]);
-        console.log("Nombre: " + json[0]["nombre"]);
-        console.log("Telefono: " + json[0]["telefono"]);
-
         const tbody_contactos = document.getElementById("tbody_contactos");
-    
-        var tr = document.createElement("tr");
-        var td_email = document.createElement("td");
-        var td_nombre = document.createElement("td");
-        var td_telefono = document.createElement("td");
 
-        td_email.innerHTML = json[1]["email"];
-        td_nombre.innerHTML = json[1]["nombre"];
-        td_telefono.innerHTML = json[1]["telefono"];
+        // Limpia el contenido existente en tbody_contactos
+        tbody_contactos.innerHTML = "";
 
-        tr.appendChild(td_email);
-        tr.appendChild(td_nombre);
-        tr.appendChild(td_telefono);
+        for (let i = 0; i < json.length; i++) {
+            const contacto = json[i];
 
-        tbody_contactos.appendChild(tr);
+            var tr = document.createElement("tr");
+            var td_email = document.createElement("td");
+            var td_nombre = document.createElement("td");
+            var td_telefono = document.createElement("td");
 
+            td_email.innerHTML = contacto["email"];
+            td_nombre.innerHTML = contacto["nombre"];
+            td_telefono.innerHTML = contacto["telefono"];
+
+            tr.appendChild(td_email);
+            tr.appendChild(td_nombre);
+            tr.appendChild(td_telefono);
+
+            tbody_contactos.appendChild(tr);
+        }
     };
-};
+}
